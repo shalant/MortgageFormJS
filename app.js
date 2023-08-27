60958c5391eb475bc3a33770d52981aa0319bf38
shalant <doug.rosenberg@gmail.com>
2023-08-27 16:37:13 -0500

    completed application!

--- app.js
+++ app.js
@@ -1,11 +1,73 @@
-const amountInput = document.getElementById('amount-input');
-const interestRateInput = document.getElementById('interest-rate-input');
-const lengthOfLoanInput = document.getElementById('length-of-loan-input');
+const loanAmount = document.getElementById('loan-amount');
+const interestRate = document.getElementById('interest-rate');
+const lengthOfMortgage = document.getElementById('length-of-mortgage');
 
-function calculateMortgagePayment() {
-    const borrowedMoney = amountInput.ariaValueMax;
-    const lengthOfLoan = lengthOfLoanInput.value * 12;
-    const interestRate = interestRateInput.value;
-    const calculatedInterest = interestRate / 100;
-    const interestReady = calculatedInterest /12;
+const form = document.getElementById("mortgage");
+
+function calculateMortgage(loanAmount, interestRate, numberMonthlyPayments) {
+    function percentageToDecimal(percent) {
+        return percent / 12 / 100;
+    }
+
+    interestRate = percentageToDecimal(interestRate);
+
+    function yearsToMonths(year) {
+        return year * 12;
+    }
+
+    numberMonthlyPayments = yearsToMonths(numberMonthlyPayments);
+
+    let mortgage = (interestRate * loanAmount) / (1 - Math.pow(1 + interestRate, -numberMonthlyPayments))
+
+    return parseFloat(mortgage.toFixed(2));
+}
+
+function calculateTotalPayment(loanAmount, interestRate, numberMonthlyPayments) {
+    function percentageToDecimal(percent) {
+        return percent / 12 / 100;
+    }
+
+    interestRate = percentageToDecimal(interestRate);
+
+    function yearsToMonths(year) {
+        return year * 12;
+    }
+
+    numberMonthlyPayments = yearsToMonths(numberMonthlyPayments);
+
+    let totalPayment = (interestRate * loanAmount) / (1 - Math.pow(1 + interestRate, -numberMonthlyPayments)) * numberMonthlyPayments;
+
+    return parseFloat(totalPayment.toFixed(2));
+}
+
+function calculateTotalInterestPaid(loanAmount, interestRate, numberMonthlyPayments) {
+    function percentageToDecimal(percent) {
+        return percent / 12 / 100;
+    }
+
+    interestRate = percentageToDecimal(interestRate);
+
+    function yearsToMonths(year) {
+        return year * 12;
+    }
+
+    numberMonthlyPayments = yearsToMonths(numberMonthlyPayments);
+
+    let totalInterestPaid = ((interestRate * loanAmount) / (1 - Math.pow(1 + interestRate, -numberMonthlyPayments)) * numberMonthlyPayments) - loanAmount
+    ;
+
+    return parseFloat(totalInterestPaid.toFixed(2));
+}
+
+form.onsubmit = (e) => {
+    e.preventDefault();
+    validate();
+
+    let monthlyPayment = calculateMortgage(loanAmount.value, interestRate.value, lengthOfMortgage.value);
+    let totalPayment = calculateTotalPayment(loanAmount.value, interestRate.value, lengthOfMortgage.value);
+    let totalInterestPaid = calculateTotalInterestPaid(loanAmount.value, interestRate.value, lengthOfMortgage.value);
+
+    document.getElementById("monthlyPayment").innerHTML = `${monthlyPayment}`;
+    document.getElementById("totalPayment").innerHTML = `${totalPayment}`;
+    document.getElementById("totalInterestPaid").innerHTML = `${totalInterestPaid}`;
 }
--- version1/mortgage.js
+++ version1/mortgage.js
@@ -1,2 +1,50 @@
 // Inputs / DOM elements
 
+const homeValue = document.getElementById("homeValue");
+const downPayment = document.getElementById("downPayment");
+const loanAmount = document.getElementById("loanAmount");
+const interestRate = document.getElementById("interestRate");
+const loanDuration = document.getElementById("loanDuration");
+
+const form = document.getElementById("mortgage");
+
+// console.log(homeValue)
+
+downPayment.addEventListener("keyup", () => {
+    loanAmount.value = homeValue.value - downPayment.value;
+
+    let loanAmountValue = loanAmount.value;
+    return loanAmountValue;
+})
+
+function calculateMortgage(loanAmount, interestRate, numberMonthlyPayments) {
+    function percentageToDecimal(percent) {
+        return percent /12 / 100;
+    }
+
+    interestRate = percentageToDecimal(interestRate);
+
+    function yearsToMonths(year) {
+        return year * 12;
+    }
+
+    numberMonthlyPayments = yearsToMonths(numberMonthlyPayments);
+
+    let mortgage = (interestRate * loanAmount) / (1 - Math.pow(1 + interestRate, -numberMonthlyPayments))
+
+    return parseFloat(mortgage.toFixed(2));
+}
+
+form.onsubmit = (e) => {
+    e.preventDefault();
+    validate();
+    let loanAmount = homeValue.value - downPayment.value;
+
+    let monthlyPayment = calculateMortgage(loanAmount, interestRate.value, loanDuration.value);
+
+    document.getElementById("monthlyPayment").innerHTML = `${monthlyPayment}`;
+}
+
+function validate() {
+    
+}
